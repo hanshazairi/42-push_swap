0e2c70d15737d742c8cc3ef78bf76b8fe051e3b5
Hans Hazairi <hanshazairi@gmail.com>
2021-08-27 14:57:40 +0800

    chore: finish error handling (check for duplicates & free)

---
+++ error_handlers.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_handlers.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/27 14:26:03 by hbaddrul          #+#    #+#             */
+/*   Updated: 2021/08/27 14:43:35 by hbaddrul         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <limits.h>
+#include <stdlib.h>
+#include "libft/libft.h"
+
+static int	isvalid(char *str)
+{
+	if (*str == '+' || *str == '-')
+		++str;
+	while (*str)
+	{
+		if (!ft_isdigit(*str++))
+			return (0);
+	}
+	return (1);
+}
+
+static int	isdistinct(int *nums)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (nums[i])
+	{
+		j = i + 1;
+		while (nums[j])
+			if (nums[i] == nums[j++])
+				return (0);
+		++i;
+	}
+	return (1);
+}
+
+void	ft_error(void)
+{
+	ft_putstr_fd("Error\n", 2);
+	exit(0);
+}
+
+int	validate1(char **strs)
+{
+	int		len;
+	char	*str;
+
+	len = 0;
+	while (strs[len])
+	{
+		str = strs[len++];
+		if (ft_atol(str) < INT_MIN || ft_atol(str) > INT_MAX || !isvalid(str))
+		{
+			free(strs);
+			ft_error();
+		}
+	}
+	return (len);
+}
+
+int	*validate2(char **strs, int len)
+{
+	int	i;
+	int	*nums;
+
+	nums = malloc(len * sizeof(int));
+	if (!nums)
+	{
+		free(strs);
+		ft_error();
+	}
+	i = -1;
+	while (++i < len)
+		nums[i] = ft_atol(strs[i]);
+	if (!isdistinct(nums))
+	{
+		free(nums);
+		free(strs);
+		ft_error();
+	}
+	return (nums);
+}
--- push_swap.c
+++ push_swap.c
@@ -6,44 +6,36 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/26 11:17:50 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/08/26 22:00:14 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/08/27 14:55:22 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <limits.h>
 #include <stdlib.h>
 #include "libft/libft.h"
 
-static void	ft_error(void)
-{
-	ft_putstr_fd("Error\n", 2);
-	exit(0);
-}
-
-static int	ft_isvalid(int c)
-{
-	return (ft_isdigit(c) || c == ' ' || c == '-');
-}
+void	ft_error(void);
+int		validate1(char **strs);
+int		*validate2(char **strs, int len);
 
 int	main(int argc, char **argv)
 {
+	int		i;
 	int		len;
-	char	*str;
+	int		*nums;
 	char	**strs;
 
 	if (argc != 2)
 		ft_error();
-	len = 0;
 	strs = ft_split(argv[1], ' ');
-	while (*strs && ++len)
+	len = validate1(strs);
+	nums = validate2(strs, len);
+	i = 0;
+	while (nums[i])
 	{
-		str = *strs;
-		if (ft_atol(str) < INT_MIN || ft_atol(str) > INT_MAX)
-			ft_error();
-		while (*str)
-			if (!ft_isvalid(*str++))
-				ft_error();
-		++strs;
+		ft_putnbr_fd(nums[i++], 1);
+		ft_putchar_fd('\n', 1);
 	}
+	free(nums);
+	free(strs);
 	return (0);
 }
